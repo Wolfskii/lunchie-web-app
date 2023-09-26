5b5617172c35c055ace264f0e485547f618f4470
Dawid Kaleta <dawid.kaleta@knightec.se>
2023-09-26 15:18:59 +0200

    Add inspo link

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import TailwindCss from './TailwindCSS.svelte';
   import Header from './components/Header.svelte';
+
+  // https://dribbble.com/shots/14820779--App
 </script>
 
 <main>
