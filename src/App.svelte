9fdb620a137a479e38488f7a3627afbb121b5075
Dawid Kaleta <dawid.kaleta@knightec.se>
2023-09-28 11:10:18 +0200

    Add weekday-menu component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import Header from './components/Header.svelte';
   import DailyMenu from './components/DailyMenu.svelte';
+  import WeekDayMenu from './components/WeekDayMenu.svelte'
 </script>
 
 <Header />
 
 <main>
+  <WeekDayMenu />
   <DailyMenu />
 </main>
 
---
+++ src/components/WeekDayMenu.svelte
@@ -0,0 +1,40 @@
+<script lang="ts"></script>
+
+  <ul class="week-day-menu">
+    <li class="day-button">
+      <a href="#">Måndag</a>
+    </li>
+    <li class="day-button">
+      <a href="#">Tisdag</a>
+    </li>
+    <li class="day-button">
+      <a href="#">Onsdag</a>
+    </li>
+    <li class="day-button">
+      <a href="#">Torsdag</a>
+    </li>
+    <li class="day-button">
+      <a href="#">Fredag</a>
+    </li>
+  </ul>
+
+<style lang="scss">
+  .week-day-menu {
+    margin-top: 2rem;
+    display: flex;
+    gap: 0.6rem;
+  }
+
+  .day-button {
+    padding: 0.5rem 0.8rem;
+    background-color: #E8AF74;
+    color: #2C3D28;
+    border-radius: 15px;
+
+    &:hover {
+      background-color: #151d13;
+      color: #E8AF74;
+      cursor: pointer;
+    }
+  }
+</style>
